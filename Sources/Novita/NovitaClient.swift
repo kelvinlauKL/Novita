8d4cf6099f8841a0a09b9dd14db62d3569643239
Kelvin Lau <kelvin.kl.lau@gmail.com>
2023-12-01 14:09:07 -0800

    package based scoped all stuff

--- Sources/Novita/NovitaClient.swift
+++ Sources/Novita/NovitaClient.swift
@@ -3,19 +3,19 @@ import OpenAPIRuntime
 import OpenAPIAsyncHTTPClient
 import Foundation
 
-public typealias Img2ImgRequest = Components.Schemas.Img2ImgRequest
-public typealias Img2ImgResponse = Components.Schemas.Img2ImgResponse
-public typealias ProgressResponse = Components.Schemas.ProgressResponse
-public typealias MergeFaceResponse = Components.Schemas.MergeFaceResponse
+package typealias Img2ImgRequest = Components.Schemas.Img2ImgRequest
+package typealias Img2ImgResponse = Components.Schemas.Img2ImgResponse
+package typealias ProgressResponse = Components.Schemas.ProgressResponse
+package typealias MergeFaceResponse = Components.Schemas.MergeFaceResponse
 
-public struct NovitaClient {
-  public enum Error: LocalizedError {
+package struct NovitaClient {
+  package enum Error: LocalizedError {
     case undocumentedResponse(description: String)
   }
   
   private let client: Client
   
-  public init(apiKey: String) {
+  package init(apiKey: String) {
     self.client = .init(
       serverURL: try! Servers.server1(),
       transport: AsyncHTTPClientTransport(configuration: .init()),
@@ -25,7 +25,7 @@ public struct NovitaClient {
     )
   }
   
-  public func getImg2Img(_ request: Img2ImgRequest) async throws -> Img2ImgResponse {
+  package func getImg2Img(_ request: Img2ImgRequest) async throws -> Img2ImgResponse {
     let input = Operations.getImg2Img.Input(body: .json(request))
     let response = try await client.getImg2Img(input)
     switch response {
@@ -36,7 +36,7 @@ public struct NovitaClient {
     }
   }
   
-  public func getProgress(taskId: String) async throws -> ProgressResponse {
+  package func getProgress(taskId: String) async throws -> ProgressResponse {
     let response = try await client.getProgress(query: .init(task_id: taskId))
     switch response {
     case .ok(let output):
@@ -46,7 +46,7 @@ public struct NovitaClient {
     }
   }
   
-  public func mergeFace(faceImageString: String, imageString: String) async throws -> MergeFaceResponse {
+  package func mergeFace(faceImageString: String, imageString: String) async throws -> MergeFaceResponse {
     let response = try await client.mergeFace(body: .json(.init(face_image_file: faceImageString, image_file: imageString)))
     switch response {
     case .ok(let output):
